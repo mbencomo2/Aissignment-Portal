5dfa1fbdb32a414d95a48c5e30c93d5e96dbd260
mbencomo2 <matt.ibencomo@gmail.com>
2022-09-21 16:15:38 -0700

    Removed document.lastmodified/updated footer

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 //This is a simple script that gets the current date and inserts it into the copyright paragraph
-const cdate = new Date(document.lastModified)
+const cdate = new Date()
 const year = cdate.getFullYear()
 
 document.querySelector("footer>h2>span").textContent = year
